a4e7d4e30a96371a16f379deec30a283825a24eb
Sam Mateosian <sam@bigroomstudios.com>
2014-09-24 13:35:52 -0400

    working on testing

--- test/index.js
+++ test/index.js
@@ -39,32 +39,4 @@ experiment('Stimpy-medium', function () {
             done();
         });
     });
-
-    
-    test('test index page', function (done) {
-        
-        // Server Config
-        var config = require('../server/config');
-        
-        // Create a server with a host, port, and options
-        var server = Hapi.createServer(config.host, config.port, config.hapi.options);
-        
-        server.pack.register([{ plugin: require("../index") }], function(err) {
-                
-            var options = {
-                method: "GET",
-                url: "/"
-            };
-         
-            server.inject(options, function(response) {
-
-                var result = response.result;
-         
-                Lab.expect(response.statusCode).to.equal(200);
-         
-                done();
-            });
-        });
-    });
-    
 });
