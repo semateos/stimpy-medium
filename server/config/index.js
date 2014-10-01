967581249df2c517339f50bb690b10ab73f3a03d
Sam Mateosian <sam@bigroomstudios.com>
2014-10-01 11:29:12 -0400

    trying out 127.0.0.1 vs 0.0.0.0

--- server/config/index.js
+++ server/config/index.js
@@ -7,7 +7,7 @@ rootPath = path.normalize(__dirname + '/../..');
 // Defaults that you can access when you require this config.
 module.exports = {
     root: rootPath,
-    host: '0.0.0.0',
+    host: '127.0.0.1',
     port: parseInt(process.env.PORT, 10) || 3000,
     hapi: {
         options: {
